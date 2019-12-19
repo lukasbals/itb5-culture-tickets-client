986807c874415f4536096d9a8d3a4243b95bd17b
BakaBoing <baka.boing@gmail.com>
2019-12-19 15:20:52 +0100

    fix for payara server

--- src/main/java/at/fhv/td/communication/EJB.java
+++ src/main/java/at/fhv/td/communication/EJB.java
@@ -1,9 +1,7 @@
 package at.fhv.td.communication;
 
-import at.fhv.td.communication.IClientSessionFactory;
 import at.fhv.td.communication.ejb.IClientSessionFactoryRemote;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.util.Properties;
@@ -17,14 +15,11 @@ public class EJB {
 
         try {
             Properties props = new Properties();
-            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty(Context.URL_PKG_PREFIXES, "com.sun.enterprise.naming");
-            props.setProperty(Context.STATE_FACTORIES, "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
-            props.setProperty(Context.PROVIDER_URL, host);
+            props.setProperty("org.omg.CORBA.ORBInitialHost", host);
+            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
 
             InitialContext ctx = new InitialContext(props);
             session = (IClientSessionFactoryRemote) ctx.lookup("at.fhv.td.communication.ejb.IClientSessionFactoryRemote");
-//            session = (IClientSessionFactory) ctx.lookup("java:global/itb5-culture-tickets-1.0-SNAPSHOT/ClientSessionFactoryBean!at.fhv.td.communication.interfaces.IClientSessionFactory");
         } catch (NamingException e) {
             e.printStackTrace();
         }
